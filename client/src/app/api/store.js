3c5a354545e12e237c09e07a3937d93cf1ae4b2c
Dale <Danntabachoyy@gmail.com>
2024-02-24 18:23:43 +0800

    successfully added redux-persist

--- client/src/app/api/store.js
+++ client/src/app/api/store.js
@@ -1,13 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './apiSlice';
 import { authSlice } from '../../features/auth/authSlice';
-const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authSlice.reducer,
-  },
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const authPersistConfig = { key: 'auth', storage };
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authSlice.reducer),
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+export const store = configureStore({
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      apiSlice.middleware
+    ),
 });
 
-export default store;
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { blogSlice } from './features/blogs/blogSlice.js';
-import store from './app/api/store.js';
+import { store, persistor } from './app/api/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 store.dispatch(blogSlice.endpoints.getBlogs.initiate());
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
